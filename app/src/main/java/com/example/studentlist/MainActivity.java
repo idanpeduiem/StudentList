6abe8a582d51cb7139ae244b9b4969e341552113
idanpeduiem <62710926+idanpeduiem@users.noreply.github.com>
2022-12-14 18:02:17 +0200

    init again

--- app/src/main/java/com/example/studentlist/MainActivity.java
+++ app/src/main/java/com/example/studentlist/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.studentlist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.studentlist.model.DataModel;
+import com.example.studentlist.model.Student;
+
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    List<Student> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        data = DataModel.instance().getAllStudents();
+        RecyclerView list = findViewById(R.id.studentlistfrag_list);
+        list.setHasFixedSize(true);
+        list.setLayoutManager(new LinearLayoutManager(this));
+        StudentRecyclerAdapter adapter = new StudentRecyclerAdapter(getLayoutInflater(),data);
+        list.setAdapter(adapter);
+
+        adapter.setOnItemClickListener(new StudentRecyclerAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int pos) {
+                Log.d("TAG", "Row was clicked " + pos);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/studentlist/model/DataModel.java
@@ -0,0 +1,30 @@
+package com.example.studentlist.model;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class DataModel {
+    private static final DataModel _instance = new DataModel();
+
+    public static DataModel instance() {
+        return _instance;
+    }
+
+    private DataModel() {
+        for (int i = 0; i < 20; i++) {
+            this.addStudent(new Student("name " + i, "" + i, "", false));
+        }
+    }
+
+    List<Student> data = new LinkedList<>();
+
+    public List<Student> getAllStudents() {
+        return data;
+    }
+
+    public void addStudent(Student st) {
+        data.add(st);
+    }
+
+
+}
